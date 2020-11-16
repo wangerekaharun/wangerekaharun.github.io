a96f4b1c73926fd97d5ffb25df6002f33d52330b
Harun Wangereka <harunwangereka@gmail.com>
2020-11-17 00:47:58 +0300

    use javadoc

--- scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":FreshlyPressed:dokkaHtml/androidTestRelease": [], ":FreshlyPressed:dokkaHtml/debug": [], ":FreshlyPressed:dokkaHtml/main": [], ":FreshlyPressed:dokkaHtml/release": []}'
