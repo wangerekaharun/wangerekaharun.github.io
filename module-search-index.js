ef7b19e8715cf6def9b8d75409800b0162ff70a4
Harun Wangereka <harunwangereka@gmail.com>
2020-11-17 00:44:11 +0300

    docs

--- module-search-index.js
+++
@@ -1,1 +0,0 @@
-var moduleSearchIndex = [{"l":"FreshlyPressed","url":"index.html"}]
---
+++ scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":FreshlyPressed:dokkaHtml/androidTestRelease": [], ":FreshlyPressed:dokkaHtml/debug": [], ":FreshlyPressed:dokkaHtml/main": [], ":FreshlyPressed:dokkaHtml/release": []}'
